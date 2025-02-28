c2eabfafea7631455ceed13e3d90d784c10f0c24
PakalaAkhila <akkiakhila620@gmail.com>
2025-02-28 16:33:46 +0530

    Add large files using Git LFS

---
+++ FakeNewsIdentification/FakeNewsIdentification/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for welfake
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FakeNewsIdentification.settings')
+
+application = get_asgi_application()
